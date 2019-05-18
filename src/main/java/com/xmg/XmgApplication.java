dc04e669caece824b98e5e2c250072e3ac4f5282
mayunning <408218852@qq.com>
2019-05-18 10:36:33 +0800

    创建自己的框架仓库

---
+++ src/main/java/com/xmg/XmgApplication.java
@@ -0,0 +1,13 @@
+package com.xmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmgApplication.class, args);
+    }
+
+}
